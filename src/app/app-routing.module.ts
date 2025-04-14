9e45842d0747bb5c4ad6e8711067df755ab1cca3
Marcos Renato Falconi <falconi.2050@gmail.com>
2025-04-14 09:34:58 -0300

    versao 14 04 2025

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   {
     path: 'outrapagina',
     loadChildren: () =>
       import('./modules/pagina01/pagina01.module').then((m) => m.Pagina01Module)
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
   },
 ];
 
